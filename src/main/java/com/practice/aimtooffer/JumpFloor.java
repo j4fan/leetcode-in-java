b244604dc52cd4a4dde557f53e037f5c3addc229
j4fan <fspirit@yeah.net>
2018-11-06 13:25:46 +0800

    add jumpfloor
    Signed-off-by: j4fan <fspirit@yeah.net>

---
+++ src/main/java/com/practice/aimtooffer/JumpFloor.java
@@ -0,0 +1,16 @@
+package com.practice.aimtooffer;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ */
+public class JumpFloor {
+    public int jump(int target) {
+        if (target == 1) {
+            return 1;
+        } else if (target == 2) {
+            return 2;
+        } else {
+            return jump(target - 1) + jump(target - 2);
+        }
+    }
+}
