9528243e65c34d402e8b230e84d763cc65c9e2d8
j4fan <fspirit@yeah.net>
2018-11-06 19:42:14 +0800

    add reversePrintList
    Signed-off-by: j4fan <fspirit@yeah.net>

---
+++ src/main/java/com/practice/aimtooffer/ReversePrintList.java
@@ -0,0 +1,27 @@
+package com.practice.aimtooffer;
+
+import java.util.ArrayList;
+
+/**
+ * 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
+public class ReversePrintList {
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        if (listNode != null) {
+            printListFromTailToHead(listNode.next);
+            list.add(listNode.val);
+        }
+        return list;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
