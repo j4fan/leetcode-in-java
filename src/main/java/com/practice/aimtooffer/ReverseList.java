947b7af25cf82bc745af0fc1d25dcd4b9d1cfb20
Fan4J <fspirit@yeah.net>
2018-11-14 22:12:24 +0800

    add reverseList
    Signed-off-by: Fan4J <fspirit@yeah.net>

---
+++ src/main/java/com/practice/aimtooffer/ReverseList.java
@@ -0,0 +1,43 @@
+package com.practice.aimtooffer;
+
+public class ReverseList {
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        head.next = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(2);
+        head.next = second;
+        second.next = third;
+        third.next = null;
+        ListNode reverseHead = reverseList(head);
+        while (reverseHead != null) {
+            System.out.println(reverseHead.val);
+            reverseHead = reverseHead.next;
+        }
+    }
+}
