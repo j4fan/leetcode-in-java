a1d82b53b3b690245cdb0f2ecc8149b4e39a0277
Fan4J <fspirit@yeah.net>
2018-11-19 22:52:39 +0800

    add FindKthToTail
    Signed-off-by: Fan4J <fspirit@yeah.net>

---
+++ src/main/java/com/practice/aimtooffer/FindKthToTail.java
@@ -0,0 +1,34 @@
+package com.practice.aimtooffer;
+
+public class FindKthToTail {
+
+    public ListNode findKthToTail(ListNode head,int k){
+        if(head==null){
+            return null;
+        }
+        if(k<=0){
+            return null;
+        }
+        ListNode pre = head;
+        for(int i=1;i<k;i++){
+            if(head.next==null){
+                return null;
+            }
+            head = head.next;
+        }
+        while(head.next!=null){
+            head = head.next;
+            pre = pre.next;
+        }
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
