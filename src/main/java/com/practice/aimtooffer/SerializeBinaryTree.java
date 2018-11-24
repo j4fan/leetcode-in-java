7bebe2c77e15a393fe0e81a7ce1e6016f4016364
Fan4J <fspirit@yeah.net>
2018-11-24 19:36:11 +0800

    add serialize&deserializeTree
    Signed-off-by: Fan4J <fspirit@yeah.net>

---
+++ src/main/java/com/practice/aimtooffer/SerializeBinaryTree.java
@@ -0,0 +1,64 @@
+package com.practice.aimtooffer;
+
+public class SerializeBinaryTree {
+    private static int flag = -1;
+
+    public static String serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        if (root == null) {
+            sb.append("#,");
+            return sb.toString();
+        }
+        sb.append(root.val + ",");
+        sb.append(serialize(root.left));
+        sb.append(serialize(root.right));
+        return sb.toString();
+    }
+
+    public static TreeNode deserialize(String str) {
+        flag++;
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        String[] strs = str.split(",");
+        if (!strs[flag].equals("#")) {
+            TreeNode node = new TreeNode(Integer.parseInt(strs[flag]));
+            node.left = deserialize(str);
+            node.right = deserialize(str);
+            return node;
+        }
+        return null;
+    }
+
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode head = new TreeNode(8);
+        TreeNode left = new TreeNode(6);
+        TreeNode right = new TreeNode(6);
+        head.left = left;
+        TreeNode leftleft = new TreeNode(5);
+        TreeNode leftright = new TreeNode(7);
+        TreeNode rightleft = new TreeNode(7);
+        TreeNode rightright = new TreeNode(5);
+        left.left = leftleft;
+        left.right = leftright;
+        right.left = rightleft;
+        right.right = rightright;
+
+        System.out.println(serialize(head));
+
+        TreeNode node = deserialize(serialize(head));
+        System.out.println("test");
+    }
+}
